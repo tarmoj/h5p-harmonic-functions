1d51b0d93d5737f83432375da186137e4fd93bbc
Tarmo Johannes <trmjhnns@gmail.com>
2022-06-09 08:26:54 +0300

    automiatic moving from cell to cell

--- harmonicFunctions.js
+++ harmonicFunctions.js
@@ -8,6 +8,7 @@ H5P.HarmonicFunctions = (function ($) {
 	 */
 	function C(options, id) {
 		this.$ = $(this);
+        const self = this;
 		// Extend defaults with provided options
 
 		// options: {functions , audioFile, notationImage} kirj
@@ -37,7 +38,34 @@ H5P.HarmonicFunctions = (function ($) {
 					id: "inputCell" + i + 1,
 					class: "inputCell",
 					// TODO: aria-label, jump to next cell?
-				});
+                    attr: {index: i, 'aria-label': "function "+(i+1).toString() },
+                    keyup: (event) => {
+                        const index = parseInt(event.target.getAttribute("index"));
+                        const input = event.target.value;
+                        const functionCount = this.functions.length;
+                        console.log("InputCell, key, index, input", event.key, index, input, /[t,s,d,m,T,S,D,M]/.test(event.key) );
+                        let result = true;
+                        let move = 0;
+
+                        if ( ["t","s","d","m"].includes(event.key.toLowerCase()) && index<functionCount-1 ) { // if a function key, move to next
+                           move = 1;
+                        } else if (event.key==="ArrowRight" && index<functionCount-1) {
+                            move = 1;
+                        } else if (event.key==="ArrowLeft" && index>0) {
+                            move = -1;
+                        }
+
+                        if (move!=0) {
+                            this.inputCells[index+move].focus();
+                        }
+
+                        if (event.key==='Enter') {
+                            console.log("Enter");
+                            this.checkResponse();
+                        }
+                    }
+                });
+                
 				console.log("createInputCells: ", this.functions[i], $inputCell);
 				this.inputCells[i] = $inputCell;
 				$inputDiv.append($inputCell);
@@ -53,9 +81,9 @@ H5P.HarmonicFunctions = (function ($) {
 			this.responded = true;
 			let correct = true;
 			for (let i=0; i<this.functions.length; i++) {
-				const response = this.inputCells[i].val();
+				const response = this.inputCells[i].val().toLocaleLowerCase();
 				console.log("response: ", i, this.inputCells[i].val() );
-				if (this.functions[i] === response) {
+				if (this.functions[i].toLocaleLowerCase() === response) {
 					feedBack = "Õige!";
 					this.inputCells[i].addClass("greenBorder");
 				} else {
@@ -94,29 +122,30 @@ H5P.HarmonicFunctions = (function ($) {
 
 		//audio
 		if (self.options.audioFile.length>0) {
-
-			$container.append($('<button/>', {
-					text: "MÄNGI", //set text 1 to 10
-					id: 'startButton',
-					click: function () { document.getElementById("audioPlayer").play();   }
-
-				}
-			));
-
-			$container.append($('<button/>', {
-					text: "STOP", //set text 1 to 10
-					id: 'stopButton',
-					click: function () {
-						console.log("Stop");
-						//$("#audioPlayer").pause();  //$(...).pause is not a function
-						const audioPlayer = document.getElementById("audioPlayer");
-						audioPlayer.pause();
-						audioPlayer.currentTime = 0;
-
-					}
-
-				}
-			));
+            
+            
+// 			$container.append($('<button/>', {
+// 					text: "MÄNGI", //set text 1 to 10
+// 					id: 'startButton',
+// 					click: function () { document.getElementById("audioPlayer").play();   }
+// 
+// 				}
+// 			));
+// 
+// 			$container.append($('<button/>', {
+// 					text: "STOP", //set text 1 to 10
+// 					id: 'stopButton',
+// 					click: function () {
+// 						console.log("Stop");
+// 						$("#audioPlayer").pause();  //$(...).pause is not a function
+// 						const audioPlayer = document.getElementById("audioPlayer");
+// 						audioPlayer.pause();
+// 						audioPlayer.currentTime = 0;
+// 
+// 					}
+// 
+// 				}
+// 			));
 
 			// TODO: create audio element with jQuery? Is it easier?
 			const relativeAudioFilePath = self.options.audioFile[0].path;
@@ -129,18 +158,20 @@ H5P.HarmonicFunctions = (function ($) {
 			audio.appendChild(source);
 			$container.append(audio);
 
-			$container.append('<br>')
 			$container.append( [
+                '<br>',    
 				$('<div>').text("Enter functions:"),
 				self.createInputCells(),
 				$('<button/>', {
 					text: "KONTROLLI",
 					id: 'checkButton',
-					click: function () {
+                    class: 'button',
+					click:  ()  => {
 						console.log("Check", self.options);
 						self.checkResponse();
 					}
-				})
+				}),
+                '<div />'
 			] );
 
 
